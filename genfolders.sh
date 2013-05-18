2d04836746280c385294925c85c04bcc18c43c78
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-05-18 15:40:55 +0300

    Folder generation script for the scala-project and it's modules

---
+++ genfolders.sh
@@ -0,0 +1,4 @@
+mkdir -p infrastructure/{project,src/{main/{resources,scala},test/scala}}
+mkdir -p domain/{project,src/{main/{resources,scala},test/scala}}
+mkdir -p application/{project,src/{main/{resources,scala,webapp/WEB-INF},test/scala}}
+touch application/src/main/webapp/WEB-INF/web.xml
