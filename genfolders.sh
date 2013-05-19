f8ad93f82c728fa8a4af0935e71f5dcab3c9aab2
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-05-19 13:29:22 +0300

    Genfolders script to generate the server

--- genfolders.sh
+++ genfolders.sh
@@ -1,4 +1,5 @@
 mkdir -p infrastructure/{src/{main/{resources,scala},test/scala}}
 mkdir -p domain/{src/{main/{resources,scala},test/scala}}
-mkdir -p application/{src/{main/{resources,scala,webapp/WEB-INF},test/scala}}
+mkdir -p application/{src/{main/{resources,scala/jetty,webapp/WEB-INF},test/scala}}
+mv Server.scala application/src/test/scala/jetty
 touch application/src/main/webapp/WEB-INF/web.xml
