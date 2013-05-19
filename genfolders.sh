698e5745f3990c0fbd7d527b667460069b3c4d8b
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-05-19 13:57:22 +0300

    Initial web.xml copying to correct location for Jetty

--- genfolders.sh
+++ genfolders.sh
@@ -3,4 +3,4 @@ mkdir -p domain/src/{main/{resources,scala},test/scala}
 mkdir -p application/src/{main/{resources,scala/jetty,webapp/WEB-INF},test/scala/jetty}
 
 mv Server.scala application/src/test/scala/jetty/Server.scala
-touch application/src/main/webapp/WEB-INF/web.xml
+mv web.xml application/src/main/webapp/WEB-INF/web.xml
