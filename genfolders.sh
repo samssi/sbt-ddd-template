347ee651a367f9467d35d75267a1a7671f8f5805
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-05-19 13:48:16 +0300

    Duh!

--- genfolders.sh
+++ genfolders.sh
@@ -1,6 +1,6 @@
 mkdir -p infrastructure/src/{main/{resources,scala},test/scala}
 mkdir -p domain/src/{main/{resources,scala},test/scala}
-mkdir -p application/src/{main/{resources,scala/jetty,webapp/WEB-INF},test/scala}
+mkdir -p application/src/{main/{resources,scala/jetty,webapp/WEB-INF},test/scala/jetty}
 
-mv Server.scala application/src/test/scala/jetty
+mv Server.scala application/src/test/scala/jetty/Server.scala
 touch application/src/main/webapp/WEB-INF/web.xml
